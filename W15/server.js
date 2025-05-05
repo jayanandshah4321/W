d51e9c34b7016551bfbfa535e29702006ece5b07
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-05-06 01:28:20 +0530

    Final Commit

--- W15/server.js
+++ W15/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const cors = require('cors')
 
+app.use(cors())
 // Serve static files from "public"
 app.use(express.static('public'));
 
 // Serve product data from JSON
 app.get('/api/products', (req, res) => {
-  const filePath = path.join(__dirname, 'products.json');
+  const filePath = path.join(__dirname, 'product.json');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) return res.status(500).json({ error: 'Failed to read product data' });
     const products = JSON.parse(data);
